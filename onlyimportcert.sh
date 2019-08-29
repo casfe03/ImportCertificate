1ddf8935f0336e127401e5eac04d16a58ffd5bf8
FelipeAPM <35493876+casfe03@users.noreply.github.com>
2019-08-29 10:58:08 -0300

    Create onlyimportcert.sh

---
+++ onlyimportcert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright 2016 Felipe Castro <fscastro@hotmail.com.br>
+# Uso: import-cert.sh <diretório do JDK>
+# Para o funcionamento correto deve ser utilizado uma JDK sem alterações no cacerts
+#
+
+declare -A HOSTS
+HOSTS["cnj"]=www.cnj.jus.br
+HOSTS["correios"]=apps.correios.com.br
+
+	for key in ${!HOSTS[@]}; do
+    echo importando ${key} ${HOSTS[${key}]} ...
+    openssl s_client -servername ${key} -connect ${HOSTS[${key}]}:443 </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' >${key}.pem
+done
