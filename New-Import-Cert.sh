255418566e159c6932194ad69bb4bb2c89db9d45
FelipeAPM <35493876+casfe03@users.noreply.github.com>
2019-08-29 10:28:00 -0300

    Update New-Import-Cert.sh

--- New-Import-Cert.sh
+++ New-Import-Cert.sh
@@ -1,7 +1,19 @@
-java -jar maven-repository-provisioner-1.4.2-SNAPSHOT-jar-with-dependencies.jar -a "activation:activation:*:1.0.2" -t "http://172.16.2.73:8081/nexus/repository/apache-maven/" -u admin -p admin123 -s "http://maven/nexus/repository/apache-maven/" -su tecnisys -sp tecnisys
-	 
-	 
-	 sudo docker tag myimage registry/desenvolvimento/incom:tag
-	 
-	 
-	 mvn clean -U package -Dproject.build.sourceEncoding=ISO-8859-1
+#!/bin/bash
+#
+# Copyright 2016 Felipe Castro <fscastro@hotmail.com.br>
+# Uso: import-cert.sh <diretório do JDK>
+# Para o funcionamento correto deve ser utilizado uma JDK sem alterações no cacerts
+#
+
+declare -A HOSTS
+HOSTS["cnj"]=www.cnj.jus.br
+HOSTS["correios"]=apps.correios.com.br
+
+for key in ${!HOSTS[@]}; do
+    echo importando ${key} ${HOSTS[${key}]} ...
+    openssl s_client -servername ${key} -connect ${HOSTS[${key}]}:443 </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' >${key}.pem
+
+	echo comparando os dois arquivos 
+	$some_var="$(diff "${key}.pem" "cnj2.pem")"
+	echo $some_var 
+done
